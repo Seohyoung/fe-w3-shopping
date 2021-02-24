2832d7631d79455402b19fcb126e39cce5ab0d0b
Seohyoung <prophecy803@gmail.com>
2021-02-24 23:21:34 +0900

    Fix typo

--- public/javascripts/carousel.js
+++ public/javascripts/carousel.js
@@ -15,8 +15,9 @@ const getJsonData = () => {
       const html = promotionJson
         .map((i) => `<img src="${i.imgurl}"/>`)
         .join('');
-      console.log(html);
       $promotionImg.innerHTML = html;
+      //현재 이미지 앞뒤로 세개 씩 불러올 수 있도록 처리..
+      //기능 분리 필요할 것 같음..
     });
 };
 
